c94a14adf4e4ecbe4d0e92da450ebd0adea01803
github-main-user <kolya.skripn@mail.ru>
2023-04-06 20:30:44 +0300

    init (too many bugs)

---
+++ main.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <math.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+static const char* GRADIENT = ".`,:;i1|tLs_-+^~ \"I<>l?7jvcft/\\()=~#<>Xz{[]}$bmwqpdbkhao*#MW&8%B@$";
+const uint8_t GRAD_LENGTH = get_str_lenght(GRADIENT);
+const uint32_t MAX_ITERS = 256;
+
+uint8_t get_str_lenght(char* str)
+{
+	uint8_t i = 0;
+	while (*str != '\0')
+	{
+		i++;
+		str++;
+	}
+	return i;
+}
+
+uint16_t* get_terminal_size()
+{
+	static struct winsize w;
+	ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+
+	static uint16_t size[2];
+	size[0] = w.ws_col;
+	size[1] = w.ws_row;
+
+	return size;
+}
+
+double convert_number(uint32_t number, uint32_t max)
+{
+	return (number - ((double)max / 2.0)) / ((double)max / 4.0);
+}
+
+char** calculate_set(width, height)
+{
+	char** set = [width][height];
+
+	for (uint16_t y = 0; y < height; y++)
+	{
+		img = convert_number(y, height);
+		for (uint16_t x = 0; x < width; x++)
+		{
+			real = convert_number(x, width);
+
+			double a = real;
+			double b = img;
+
+			double orig_a = a;
+			double orig_b = b;
+
+			uint32_t it = 0;
+			do
+			{
+				it++;
+				to_square(&a, &b, &orig_a, &orig_b);
+				if (magn(a, b) > 2.0) break;
+
+			} while (it < MAX_ITERS);
+
+			set[x][y] = it / (MAX_ITERS / GRAD_LENGTH);
+		}
+	}
+	return set;	
+}
+
+void to_square(double* a, double* b, double* orig_a, double* orig_b)
+{
+	double temp = (*a * *a) - (*b * *b);
+	*b = 2.0 * *a * *b;
+	*a = temp;
+
+	*a += *orig_a;
+	*b += *orig_b;
+}
+
+double magn(double a, double b)
+{
+	return sqrt(a * a + b * b)
+}
+	
+
+int main()
+{
+	char s = '#';
+
+	uint16_t* size = get_terminal_size();
+	uint16_t width = size[0];
+	uint16_t height = size[1];
+
+
+
+	printf(symbs);
+	return 0;
+
+}
+
+
