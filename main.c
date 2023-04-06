6867214145610c7bc9e7d41d33a15de6886367ea
github-main-user <kolya.skripn@mail.ru>
2023-04-06 22:23:16 +0300

    static set

--- main.c
+++ main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include <math.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
 
-static const char* GRADIENT = ".`,:;i1|tLs_-+^~ \"I<>l?7jvcft/\\()=~#<>Xz{[]}$bmwqpdbkhao*#MW&8%B@$";
-const uint8_t GRAD_LENGTH = get_str_lenght(GRADIENT);
+const char* GRADIENT = ".`,:;i1|tLs_-+^~ \"I<>l?7jvcft/\\()=~#<>Xz{[]}$bmwqpdbkhao*#MW&8%B@$";
 const uint32_t MAX_ITERS = 256;
 
-uint8_t get_str_lenght(char* str)
+uint8_t get_str_length(const char* str)
 {
 	uint8_t i = 0;
 	while (*str != '\0')
@@ -36,16 +36,32 @@ double convert_number(uint32_t number, uint32_t max)
 	return (number - ((double)max / 2.0)) / ((double)max / 4.0);
 }
 
-char** calculate_set(width, height)
+void to_square(double* a, double* b, double* orig_a, double* orig_b)
 {
-	char** set = [width][height];
+	double temp = (*a * *a) - (*b * *b);
+	*b = 2.0 * *a * *b;
+	*a = temp;
+
+	*a += *orig_a;
+	*b += *orig_b;
+}
+
+double magn(double a, double b)
+{
+	return sqrt(a * a + b * b);
+}
+
+char* calculate_set(uint16_t width, uint16_t height)
+{
+	uint8_t grad_length = get_str_length(GRADIENT);
+	char* set = malloc(width * height);
 
 	for (uint16_t y = 0; y < height; y++)
 	{
-		img = convert_number(y, height);
+		double img = convert_number(y, height);
 		for (uint16_t x = 0; x < width; x++)
 		{
-			real = convert_number(x, width);
+			double real = convert_number(x, width);
 
 			double a = real;
 			double b = img;
@@ -60,28 +76,16 @@ char** calculate_set(width, height)
 				to_square(&a, &b, &orig_a, &orig_b);
 				if (magn(a, b) > 2.0) break;
 
-			} while (it < MAX_ITERS);
+			} while (it < MAX_ITERS - 1);
 
-			set[x][y] = it / (MAX_ITERS / GRAD_LENGTH);
+			uint8_t val = (uint32_t)(grad_length * ((double)it / MAX_ITERS));
+			set[width * y + x] = GRADIENT[val];
+			printf("it = %d, val = %f\n", it,(float)val);
 		}
 	}
 	return set;	
 }
 
-void to_square(double* a, double* b, double* orig_a, double* orig_b)
-{
-	double temp = (*a * *a) - (*b * *b);
-	*b = 2.0 * *a * *b;
-	*a = temp;
-
-	*a += *orig_a;
-	*b += *orig_b;
-}
-
-double magn(double a, double b)
-{
-	return sqrt(a * a + b * b)
-}
 	
 
 int main()
@@ -92,9 +96,9 @@ int main()
 	uint16_t width = size[0];
 	uint16_t height = size[1];
 
+	char* set = calculate_set(width, height);
+	printf(set);
 
-
-	printf(symbs);
 	return 0;
 
 }
